7b646c1a729aac5432e8fd770338421edfbe3bc1
Mohamed Deiab <63822100+m07med176@users.noreply.github.com>
2023-01-07 09:38:35 +0200

    Details Acivity

--- app/src/main/java/com/udacity/DetailActivity.kt
+++ app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,8 +1,11 @@
 package com.udacity
 
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.content_detail.*
 
 class DetailActivity : AppCompatActivity() {
 
@@ -10,6 +13,22 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         setSupportActionBar(toolbar)
+        fetchDataFromIntent();
+        handleBtns();
+    }
+
+    private fun handleBtns() {
+        ok_btn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+
+    private fun fetchDataFromIntent() {
+        statusText.text = intent.getStringExtra("status").also {
+            filename.text = intent.getStringExtra("filename")
+            if(it == "failed") statusText.setTextColor(Color.RED) else statusText.setTextColor(Color.GREEN)
+        }
     }
 
 }
